c233a0885ae6b98d5d2bd1efa768a5d630d16303
Colo <colo@MacBook-Pro-de-Colo.local>
2026-03-01 17:56:12 -0300

    Actualiza hook para comunicacion via websocket a una room

--- hooks/useWebSocket.ts
+++ hooks/useWebSocket.ts
@@ -4,11 +4,30 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 const NEST_WS_URL = "http://localhost:3001";
+const ROOM_NAME = "person-detector-app"
 
 export function useWebSocket(onConfigUpdate: () => void) {
     useEffect(()=> {
 
         const socket = io(NEST_WS_URL);
+
+        socket.on("connect", () => {
+            console.log(`OK! WebSocket conectado al servidor NestJS`);
+
+            // Al conectase, unirse a la room definida
+            socket.emit("join", ROOM_NAME)
+        });
+
+        socket.on("disconnect", () => {
+            console.warn(`WebSocket desconectado del servidor NestJS`);
+        });
+
+        // Escuchar evento de config update
+        socket.on("config:update", () => {
+            console.log(`WebSocket: evento de actualizacion recibida`);
+            onConfigUpdate(); // En este evento llamar a la función pasada desde el componente para recargar la config (es el refresh() del Context)
+        });
+
 
         // check para test manual con el browser
         if(process.env.NODE_ENV === "development") {
@@ -24,23 +43,9 @@ export function useWebSocket(onConfigUpdate: () => void) {
             */
         }
 
-        socket.on("connect", () => {
-            console.log(`WebSocket conectado al servidor NestJS`);
-        });
-
-        socket.on("disconnect", () => {
-            console.warn(`WebSocket desconectado del servidor NestJS`);
-        });
-
-        // Escuchar evento de config update
-        socket.on("config:update", () => {
-            console.log(`WebSocket: evento de actualizacion recibida`);
-            onConfigUpdate(); // En este evento llamar a la función pasada desde el componente para recargar la config (es el refresh() del Context)
-        });
-
         return () => {
             socket.disconnect();
         }
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 }
